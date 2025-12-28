13db03863db57032a42aac9a75053241ef244e7f
xDominn <xdominn11@gmail.com>
2025-12-28 20:57:57 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ TOKEN = os.getenv("TOKEN")
 
 WELCOME_CHANNEL = "︙🛬︙witamy︙"
 LOG_CHANNEL = "︙📝︙logi︙"
+OPINIE_CHANNEL = "︙✅︙opinie︙"
+CENNIK_CHANNEL = "︙💸︙cennik︙"
+ZAMOWIENIA_CHANNEL = "︙✉️︙zamówienia︙"
 
 KLIENT_ROLE = "KLIENT"
 GRAFIK_ROLE = "GRAFIK"
+NOWA_ROLA = "CZŁONEK"
