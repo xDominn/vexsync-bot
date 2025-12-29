16954e404311d7876de37387bd5d699d80c90995
xDominn <xdominn11@gmail.com>
2025-12-29 10:27:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,15 @@ import os
 
 TOKEN = os.getenv("TOKEN")
 
+# Kanały
 WELCOME_CHANNEL = "︙🛬︙witamy︙"
 LOG_CHANNEL = "︙📝︙logi︙"
 OPINIE_CHANNEL = "︙✅︙opinie︙"
 CENNIK_CHANNEL = "︙💸︙cennik︙"
 ZAMOWIENIA_CHANNEL = "︙✉️︙zamówienia︙"
 
+# Role
 KLIENT_ROLE = "KLIENT"
 GRAFIK_ROLE = "GRAFIK"
+MONTAZ_ROLE = "MONTAŻYSTA"
 NOWA_ROLA = "CZŁONEK"
